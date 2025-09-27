ac4a6dbf936146b8426252afd7af8f9b999b33c4
aimanyusufw <aimanyusufdev@gmail.com>
2025-09-27 12:27:28 +0700

    feat : setup main database formay

---
+++ database/migrations/2025_09_24_144501_create_advocates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('advocates', function (Blueprint $table) {
+            $table->id();
+            $table->string('photo')->nullable();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('specialization')->nullable();
+            $table->string('license_number')->nullable();
+            $table->string('firm_name')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('advocates');
+    }
+};
---
+++ database/migrations/2025_09_24_144503_client_cases.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cases', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained('clients')->onDelete('cascade');
+            $table->foreignId('advocate_id')->constrained('advocates')->onDelete('cascade');
+            $table->string('case_type');
+            $table->text('property_location')->nullable();
+            $table->string('urgency_level')->default('normal');
+            $table->decimal('client_budget', 10, 2)->nullable();
+            $table->text('case_details')->nullable();
+            $table->text('internal_notes')->nullable();
+            $table->string('status')->default('open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cases');
+    }
+};
