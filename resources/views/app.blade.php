d12787ecb511c00a08c2c44d3e007428c0453723
aimanyusufw <aimanyusufdev@gmail.com>
2025-09-29 20:50:06 +0700

    feat : setup react inertia

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+    @inertiaHead
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
