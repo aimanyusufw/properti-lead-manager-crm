bcd111e615cc430ae78f3f5e485d438dc2da9ead
aimanyusufw <aimanyusufdev@gmail.com>
2025-10-05 10:47:52 +0700

    ref : change padding from 6 to 5

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
     return (
         <>
             <nav
-                className={`py-4 md:py-6 fixed top-0 left-0 right-0 z-[50] transition-all duration-300 ${
+                className={`py-4 md:py-5 fixed top-0 left-0 right-0 z-[50] transition-all duration-300 ${
                     navScrol ? "navbar-fixed" : ""
                 }`}
             >
