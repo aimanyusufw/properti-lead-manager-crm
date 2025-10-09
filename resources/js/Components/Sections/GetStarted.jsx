438c32bccee6465fe37112e7dc3be352b45d2039
Aiman Yusuf Wicaksono <154625752+aimanyusufw@users.noreply.github.com>
2025-10-09 22:05:17 +0700

    ref : change text in button

--- resources/js/Components/Sections/GetStarted.jsx
+++ resources/js/Components/Sections/GetStarted.jsx
@@ -163,7 +163,7 @@ const GetStarted = () => {
                                 type="submit"
                                 className="text-white bg-primary hover:bg-primary/90 focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                             >
-                                Register new account
+                                Send Your Case
                             </button>
                         </div>
                     </div>
